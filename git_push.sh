76226653b3b3706be2fc3d278fd1a1400709a5aa
bvdvlg <bakumov.vd@phystech.edu>
2022-04-02 17:16:35 +0300

    Init

---
+++ git_push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COMMENT=$2 || "s"
+
+for file in /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.secret;do
+len=${#file}
+gpg --batch --output ${file::len-7}.gpg --passphrase $1 --symmetric $file;done
+rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.secret
+git add templates helpers.py api key_storage templates tgServer tgServer manage.py git_push.sh git_pull.sh
+git commit -m $COMMENT
+for file in key_storage/*;do
+echo $file;done
+for file in /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg;do
+len=${#file}
+gpg --batch --output ${file::len-4}.secret --passphrase $1 --decrypt $file;done
+rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg
+git push --force
