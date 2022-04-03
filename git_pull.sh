faf0c45d36157cc838726fdef3700040269e9403
bvdvlg <bakumov.vd@phystech.edu>
2022-04-03 12:49:51 +0300

    Init

--- git_pull.sh
+++ git_pull.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.secret
+rm key_storage/*.secret
 git reset --hard
 git pull origin main
-for file in /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg;do
+for file in key_storage/*.gpg;do
 len=${#file}
 gpg --batch --output ${file::len-4}.secret --passphrase $1 --decrypt $file;done
-rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg
+rm key_storage/*.gpg
--- git_push.sh
+++ git_push.sh
@@ -2,16 +2,16 @@
 
 COMMENT=$2 || "s"
 
-for file in /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.secret;do
+for file in key_storage/*.secret;do
 len=${#file}
 gpg --batch --output ${file::len-7}.gpg --passphrase $1 --symmetric $file;done
-rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.secret
+rm key_storage/*.secret
 git add templates helpers.py api/* tgServer/*.py tgServer/*.py manage.py git_push.sh git_pull.sh
 git commit -m $COMMENT
 for file in key_storage/*;do
 echo $file;done
-for file in /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg;do
+for file in key_storage/*.gpg;do
 len=${#file}
 gpg --batch --output ${file::len-4}.secret --passphrase $1 --decrypt $file;done
-rm /Users/bvdvlg/PycharmProjects/tgServer/key_storage/*.gpg
+rm key_storage/*.gpg
 git push --force
